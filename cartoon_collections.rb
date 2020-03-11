5a1702574508448241dcde6f00cc52b10c6a9c83
Alejandra I. Lee Vargas <leevargasale@gmail.com>
2020-03-11 04:31:00 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,7 +17,6 @@ end
 
 def find_the_cheese(list)
   cheese_types = ["cheddar", "gouda", "camembert"]
- # list.detect{ |name| name=="cheddar" || name=="gouda" || name=="camembert"}
   
   list.detect{ |item| cheese_types.include?(item)}
 end
