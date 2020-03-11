851ddf9973e99498d3224de66c0030c981da0d7e
Alejandra I. Lee Vargas <leevargasale@gmail.com>
2020-03-11 04:10:18 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,9 +11,8 @@ def summon_captain_planet(planeteer_calls)
 end
 
 def long_planeteer_calls(array)
-  array.any? |array| do
-    array.length>4
-  end
+  array.any? {|array| array.length>4}
+  
 end
 
 def find_the_cheese# code an argument here
