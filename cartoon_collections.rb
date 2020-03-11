0dddb777dbc6b78fefad4a17cce2e64a6bbecfc8
Alejandra I. Lee Vargas <leevargasale@gmail.com>
2020-03-11 04:22:48 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,5 +17,5 @@ end
 
 def find_the_cheese(list)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  list.detect{ |name|
+  list.detect{ |name| name=="cheddar" || name=="gouda" || name=="camembert"}
 end
